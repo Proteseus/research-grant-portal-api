e857121f3737f20a28d9ba97e2b4654260d87cb6
Proteseus <lewiwui@gmail.com>
2025-03-20 22:16:23 +0300

    cors update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: [process.env.CORS_ORIGIN, 'http://localhost:5173'],
   credentials: true
 }));
 app.use(morgan('dev'));
